5fc8669b729d4f7fcb964fd7768d59e624943b42
chenxue <melthaw@gmail.com>
2019-10-30 10:23:31 +0800

    add more test

--- module/test/src/test/java/in/clouthink/daas/es6/test/suites/HttpRequestEventRepositoryTest.java
+++ module/test/src/test/java/in/clouthink/daas/es6/test/suites/HttpRequestEventRepositoryTest.java
@@ -6,6 +6,7 @@ import in.clouthink.daas.es6.test.common.AbstractTest;
 import in.clouthink.daas.es6.test.model.HttpRequestEvent;
 import in.clouthink.daas.es6.test.repository.HttpRequestEventRepository;
 import in.clouthink.daas.es6.test.request.DefaultHttpRequestEventSearchRequest;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -21,7 +22,41 @@ public class HttpRequestEventRepositoryTest extends AbstractTest {
     @Autowired
     private HttpRequestEventRepository eventRepository;
 
+
+    //    @Test
+    public void testCreateHttpRequestEvent() {
+
+        for (int i = 0; i < 100; i++) {
+            HttpRequestEvent newData = new HttpRequestEvent();
+            newData.setRealm("daas-es6");
+            newData.setRemoteAddr("mock-remote-addr");
+            newData.setRealIp("127.0.0.1");
+            newData.setUserAgent("mock-user-agent");
+            newData.setRequestMethod("GET");
+            newData.setRequestBody("<mock-body/>" + RandomStringUtils.random(100));
+            newData.setRequestAt(new Date());
+            newData.setResponseStatus("404");
+            newData.setResponseBody("Not found");
+            newData.setAuthorization("bearer " + RandomStringUtils.random(20));
+            newData.setUsername("admin" + RandomStringUtils.random(2));
+            newData.setTimeConsuming(System.currentTimeMillis());
+
+            String id = eventRepository.save(newData);
+        }
+
+    }
+
     @Test
+    public void testPageableSearch() {
+        //Now test search with fields specified
+        Page<HttpRequestEvent> eventPage = eventRepository.getAll(new DefaultPageableSearchRequest(),
+                                                                  Fields.empty()
+                                                                        .excludes("responseBody"));
+
+        System.out.println(eventPage.getTotalElements());
+    }
+
+    //    @Test
     public void testHttpRequestEventRepo() {
         Page<HttpRequestEvent> eventPage = eventRepository.search(new DefaultHttpRequestEventSearchRequest());
         long totalElements = eventPage.getTotalElements();
